91a1bae05e635bf8f55718fa75986740f5e57507
Leo <leonid.samograew@gmail.com>
2022-02-23 20:01:21 +0300

    Nav scss

--- src/App/Nav/Nav.tsx
+++ src/App/Nav/Nav.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Nav.scss'
+import { AiFillAccountBook } from "react-icons/ai";
 
 type Props = {}
 
 export default function Nav({ }: Props) {
   return (
-    <div className='Nav'>Nav</div>
+    <div className='Nav'>
+      <div className="Nav__body">
+        <div className="Nav__top">
+          <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
+          <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
+          <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
+          <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
+        </div>
+        <div className='Nav__bottom'>
+          <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
+          <div>Nav</div>
+        </div>
+      </div>
+    </div>
   )
 }
