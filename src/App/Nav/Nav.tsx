d38869bb17647be8845e8741d1cf658eeab82887
Leo <leonid.samograew@gmail.com>
2022-04-07 01:19:41 +0300

    Nav links

--- src/App/Nav/Nav.tsx
+++ src/App/Nav/Nav.tsx
@@ -9,26 +9,33 @@ export default function Nav({ }: Props) {
   return (
     <div className='Nav'>
       <div className="Nav__body">
-        <div className="Nav__top">
-          <NavLink to={"/second-project-eng/DeepSearch"} className="Navbar-right-elem" title="Искать слова">
-            <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
-          </NavLink>
-          <NavLink to={"/second-project-eng/MyWords"} className="Navbar-right-elem" title="Искать слова">
-            <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
-          </NavLink>
-          <NavLink to={"/second-project-eng/Contacts"} className="Navbar-right-elem" title="Искать слова">
-            <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
-          </NavLink>
-          <NavLink to={"/second-project-eng/Learn"} className="Navbar-right-elem" title="Искать слова">
-            <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
-          </NavLink>
-        </div>
-        <div className='Nav__bottom'>
-          <NavLink to={"/second-project-eng/Statistics"} className="Navbar-right-elem" title="Искать слова">
-            <div className="Nav__elem"><AiFillAccountBook size={50} title="1" /></div>
-            <div>Nav</div>
-          </NavLink>
-        </div>
+        <NavLink to={"/second-project-eng/DeepSearch"} className="Navbar-right-elem" title="Продвинутый поиск">
+          <div className="Nav__elem">
+            <div className="Nav__elemicon"><AiFillAccountBook size={35} /></div>
+            <div className="Nav__elemtext">Поиск</div>
+          </div>          </NavLink>
+        <NavLink to={"/second-project-eng/MyWords"} className="Navbar-right-elem" title="Добавленные слова">
+          <div className="Nav__elem">
+            <div className="Nav__elemicon"><AiFillAccountBook size={35} /></div>
+            <div className="Nav__elemtext">Мои слова</div>
+          </div>          </NavLink>
+        <NavLink to={"/second-project-eng/Contacts"} className="Navbar-right-elem" title="Учить">
+          <div className="Nav__elem">
+            <div className="Nav__elemicon"><AiFillAccountBook size={35} /></div>
+            <div className="Nav__elemtext">Учить</div>
+          </div>          </NavLink>
+        <NavLink to={"/second-project-eng/Learn"} className="Navbar-right-elem" title="Статистика">
+          <div className="Nav__elem">
+            <div className="Nav__elemicon"><AiFillAccountBook size={35} /></div>
+            <div className="Nav__elemtext">Статистика</div>
+          </div>
+        </NavLink>
+        <NavLink to={"/second-project-eng/Statistics"} className="Navbar-right-elem" title="F.A.Q.">
+          <div className="Nav__elem">
+            <div className="Nav__elemicon"><AiFillAccountBook size={35} /></div>
+            <div className="Nav__elemtext">F.A.Q.</div>
+          </div>
+        </NavLink>
       </div>
     </div>
   )
