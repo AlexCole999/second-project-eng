4143f3f1a2a1ca81d0b1064e6d84169a530db02a
Leo <leonid.samograew@gmail.com>
2022-04-06 15:56:46 +0300

    browser-router component

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,10 @@ import App from './App/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'));
