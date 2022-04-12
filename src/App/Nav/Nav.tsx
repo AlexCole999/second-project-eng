108581f414c9bdd0f14ce8948805bb13a6e414e4
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-04-12 23:53:30 +0300

    deleted icon

--- src/App/Nav/Nav.tsx
+++ src/App/Nav/Nav.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Nav.scss'
-import { AiFillAccountBook } from "react-icons/ai";
 import { FiList } from "react-icons/fi";
 import { FiZoomIn } from "react-icons/fi";
 import { FiPlay } from "react-icons/fi";
