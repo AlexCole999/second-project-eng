b81e0b4b0a232e025e6847abe240a166e81d96a1
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-04-13 21:07:08 +0300

    languageslist

--- src/App/Nav/NavSearch/NavSearch.tsx
+++ src/App/Nav/NavSearch/NavSearch.tsx
@@ -8,24 +8,31 @@ type Props = {}
 
 export default function NavSearch({ }: Props) {
 
-  const [language, setlanguage] = useState('en-ru')
+  const languages = [
+    "bg-ru", "cs-ru", "de-ru", "en-ru",
+    "es-ru", "fr-ru", "it-ru", "nl-ru",
+    "pl-ru", "pt-ru", "sv-ru", "tr-ru",
+    "uk-ru", "zh-ru"]
+
+  const [selectedLanguage, setSelectedLanguage] = useState('ru-es')
 
   const debouncedRequest = debounce(yandexDictionaryRequest, 500)
 
   function yandexDictionaryRequest(input: string): void {
-    if (input.match(/\w+$/)) {
+    if (input.match(/[a-zA-Zа-яА-Я]+$/)) {
       fetch
         ('https://dictionary.yandex.net/api/v1/dicservice.json/lookup'
           + '?key='
           + yandexDictionaryKey
           + '&lang='
-          + language
+          + selectedLanguage
           + '&text='
           + input)
         .then(x => x.json())
         .then(x => console.log(x));
     }
   }
+
   return (
     <div className="NavSearch">
       <input placeholder="..."
@@ -34,7 +41,14 @@ export default function NavSearch({ }: Props) {
           (e) => {
             debouncedRequest(e.target.value);
           }} />
-      <div className="languange">ENG</div>
+      <div className="NavSearch__languages">
+        <div className="NavSearch__selectedLanguage">
+          {selectedLanguage}
+        </div>
+        <div className="NavSearch__languageList">
+          {languages.map(x => <div className="NavSearch__languageListElem">{x}</div>)}
+        </div>
+      </div>
     </div>
   )
 }
