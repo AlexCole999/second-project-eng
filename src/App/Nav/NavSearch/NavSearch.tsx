139e9a12a530a4168ce4e09901b26312515836b4
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-04-13 19:19:12 +0300

    deleted useless inpur checker in yandexDictionaruRequest fucntion

--- src/App/Nav/NavSearch/NavSearch.tsx
+++ src/App/Nav/NavSearch/NavSearch.tsx
@@ -3,27 +3,27 @@ import { useState } from 'react';
 import yandexDictionaryKey from '../../../API/yandexDictionaryKey';
 import debounce from './../../../functions/debounce';
 import './NavSearch.scss';
+
 type Props = {}
 
 export default function NavSearch({ }: Props) {
+
   const [language, setlanguage] = useState('en-ru')
 
   const debouncedRequest = debounce(yandexDictionaryRequest, 500)
 
   function yandexDictionaryRequest(input: string): void {
-    if (input) {
-      if (input.match(/\w+$/)) {
-        fetch
-          ('https://dictionary.yandex.net/api/v1/dicservice.json/lookup'
-            + '?key='
-            + yandexDictionaryKey
-            + '&lang='
-            + language
-            + '&text='
-            + input)
-          .then(x => x.json())
-          .then(x => console.log(x));
-      }
+    if (input.match(/\w+$/)) {
+      fetch
+        ('https://dictionary.yandex.net/api/v1/dicservice.json/lookup'
+          + '?key='
+          + yandexDictionaryKey
+          + '&lang='
+          + language
+          + '&text='
+          + input)
+        .then(x => x.json())
+        .then(x => console.log(x));
     }
   }
   return (
