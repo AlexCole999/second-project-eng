e791db0702e5b5c36e25be8fa5824492e14eea86
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-04-17 22:15:56 +0300

    language reverse and select function

--- src/App/Nav/NavSearch/NavSearch.tsx
+++ src/App/Nav/NavSearch/NavSearch.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './NavSearch.scss';
 import { useState, useRef } from 'react';
-import { FiChevronsRight } from "react-icons/fi";
+import { FiChevronsRight, FiRotateCw } from "react-icons/fi";
 import yandexDictionaryKey from '../../../API/yandexDictionaryKey';
 import ReactCountryFlag from "react-country-flag"
 import debounce from './../../../functions/debounce';
@@ -14,17 +14,13 @@ export default function NavSearch({ }: Props) {
   const [selectedLanguage, setSelectedLanguage] = useState('en-ru')
   const [selectedLanguageFlag, setSelectedLanguageFlag] = useState(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="US" style={{ width: '35px', height: '35px' }} svg />)
 
-  const languagesList = useRef(null);
   const languageListTrigger = useRef(null);
-
-  const languages = ["cs-ru", "pt-ru", "sv-ru", "tr-ru", "uk-ru", "zh-ru"]
+  const languagesList = useRef(null);
 
   function openCloseLanguagesTrigger(): void {
     languagesList.current.classList.toggle('NavSearch__languagesList_opened');
     languageListTrigger.current.classList.toggle('NavSearch__languagesListTrigger_opened');
   }
-
-  const debouncedYandexDictionaryRequest = debounce(yandexDictionaryRequest, 500)
 
   function yandexDictionaryRequest(input: string): void {
     if (input.match(/[a-zA-Zа-яА-Я]+$/)) {
@@ -41,34 +37,34 @@ export default function NavSearch({ }: Props) {
     }
   }
 
+  const debouncedYandexDictionaryRequest = debounce(yandexDictionaryRequest, 500)
+
   function selectLanguage(e) {
-    let language = e.target.parentElement.childNodes[1].outerText;
+    const language = e.target.parentElement.childNodes[1].outerText;
     setSelectedLanguage(language);
-    function languageFlagCheck(language) {
-      language == 'de-ru'
-        ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="DE" style={{ width: '35px', height: '35px' }} svg />)
-        : language == 'fr-ru'
-          ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="FR" style={{ width: '35px', height: '35px' }} svg />)
-          : language == 'es-ru'
-            ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="ES" style={{ width: '35px', height: '35px' }} svg />)
-            : language == 'it-ru'
-              ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="IT" style={{ width: '35px', height: '35px' }} svg />)
-              : language == 'nl-ru'
-                ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="NL" style={{ width: '35px', height: '35px' }} svg />)
-                : language == 'pl-ru'
-                  ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="PL" style={{ width: '35px', height: '35px' }} svg />)
-                  : language == 'bg-ru'
-                    ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="BG" style={{ width: '35px', height: '35px' }} svg />)
-                    : language == 'cs-ru'
-                      ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="CZ" style={{ width: '35px', height: '35px' }} svg />)
-                      : ""
-
-
-
-
-    }
     languageFlagCheck(language)
+  }
 
+  function languageFlagCheck(language) {
+    language == 'de-ru'
+      ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="DE" style={{ width: '35px', height: '35px' }} svg />)
+      : language == 'fr-ru'
+        ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="FR" style={{ width: '35px', height: '35px' }} svg />)
+        : language == 'es-ru'
+          ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="ES" style={{ width: '35px', height: '35px' }} svg />)
+          : language == 'it-ru'
+            ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="IT" style={{ width: '35px', height: '35px' }} svg />)
+            : language == 'nl-ru'
+              ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="NL" style={{ width: '35px', height: '35px' }} svg />)
+              : language == 'pl-ru'
+                ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="PL" style={{ width: '35px', height: '35px' }} svg />)
+                : language == 'bg-ru'
+                  ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="BG" style={{ width: '35px', height: '35px' }} svg />)
+                  : language == 'cs-ru'
+                    ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="CZ" style={{ width: '35px', height: '35px' }} svg />)
+                    : language == 'en-ru'
+                      ? setSelectedLanguageFlag(<ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="US" style={{ width: '35px', height: '35px' }} svg />)
+                      : ""
   }
 
   return (
@@ -86,14 +82,22 @@ export default function NavSearch({ }: Props) {
         <div className="NavSearch__selectedLanguage">
           <div>{selectedLanguageFlag}</div>
           <div>{selectedLanguage}</div>
+          <div className="NavSearch__reverseButtonSelectedLanguage"
+            onClick={
+              (e) => {
+                setSelectedLanguage(selectedLanguage.split('-').reverse().join('-'));
+                e.target.parentElement.classList.toggle('NavSearch__reverseButtonSelectedLanguage_reversed');
+              }
+            }>
+            <FiRotateCw />
+          </div>
         </div>
         <div className="NavSearch__languagesListTrigger" onClick={openCloseLanguagesTrigger} ref={languageListTrigger}>
           <FiChevronsRight size={20} />
         </div>
         <div className="NavSearch__languagesList"
           ref={languagesList}
-          onClick={selectLanguage}
-        >
+          onClick={selectLanguage}>
           <div className="NavSearch__languageListElem">
             <ReactCountryFlag className="NavSearch__languageListElemFlag" countryCode="US" style={{ width: '30px', height: '30px' }} svg />
             <div className="NavSearch__languageListElemText">
