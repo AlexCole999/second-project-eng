2e41764dc090f47dd127d916eeec49fa230a3c75
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-04-27 16:04:33 +0300

    logInUserWithGoogleAuthReducer for combinerReducers

--- src/App/redux/reducers/combinedReducers.js
+++ src/App/redux/reducers/combinedReducers.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import getWordsFromFirebaseReducer from './getWordsFromFirebaseReducer';
+import logInUserWithGoogleAuthReducer from './logInUserWithGoogleAuth';
 
 const combinedReducers = combineReducers({
-  wordsFromFirebase: getWordsFromFirebaseReducer
+  wordsFromFirebase: getWordsFromFirebaseReducer,
+  user: logInUserWithGoogleAuthReducer
 })
 
 export default combinedReducers;
