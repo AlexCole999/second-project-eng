bbb8b72b636c4e3c74afc1ba934caaae9cb8b1f1
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-04-28 23:51:50 +0300

    border for NavUser component + some fixes for signInWithGoogle function

--- src/App/Nav/NavUser/NavUser.tsx
+++ src/App/Nav/NavUser/NavUser.tsx
@@ -12,23 +12,18 @@ export default function NavUser({ }: Props) {
 
   const displayName = useSelector(state => state.user?.data?.user?.displayName[0])
 
-  function singInWithGooglePopup() {
-
+  const singInWithGooglePopup = () => {
     const provider = new GoogleAuthProvider();
     const auth = getAuth();
     signInWithPopup(auth, provider)
       .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
-        const user = result.user;
         dispatch({ type: "LOG_IN_USER_WITH_GOOGLEAUTH", payload: result })
       }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.email;
         const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(error);
       });
-
   }
 
   return (
@@ -37,7 +32,7 @@ export default function NavUser({ }: Props) {
         className="NavUser__name">
         {displayName !== undefined
           ? <div>{displayName}</div>
-          : <FiUserX size={20} onClick={singInWithGooglePopup} />}
+          : <FiUserX size={25} onClick={singInWithGooglePopup} />}
       </div>
     </div>
   )
