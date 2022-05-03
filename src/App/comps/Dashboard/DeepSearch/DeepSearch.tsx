8a8450d9b8107eacdc82ffd6c6a8e214303275fc
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-05-03 12:29:49 +0300

    preparing for DeepSearch display results

--- src/App/comps/Dashboard/DeepSearch/DeepSearch.tsx
+++ src/App/comps/Dashboard/DeepSearch/DeepSearch.tsx
@@ -16,13 +16,22 @@ export default function DeepSearch({ }: Props) {
       }>
 
       </button>
-      {some == undefined
-        ? "none"
-        : some.map(x =>
-          <div style={{ border: '1px solid black' }}>
-            {x.pos};
-            {x.tr.map(x => <div>{x.text}</div>)}
-          </div>)}
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        {some == undefined
+          ? "none"
+          : some.map(x =>
+            <div style={{ border: '1px solid black', margin: '15px', maxWidth: '250px', padding: '8px' }}>
+              <div style={{ display: 'flex', justifyContent: 'center' }}><div>{x.pos}</div></div>
+              {x.tr.map(x =>
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                  <div>{x.text}</div>
+                  <div>
+                    <button style={{ marginLeft: '5px', width: '25px', height: '15px', backgroundColor: "green" }}></button>
+                    <button disabled style={{ marginLeft: '5px', width: '15px', height: '15px', backgroundColor: "red" }}></button>
+                  </div>
+                </div>)}
+            </div>)}
+      </div>
     </div>
   )
 }
