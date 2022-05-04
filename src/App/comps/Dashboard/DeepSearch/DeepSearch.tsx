e16b1970ea5075890524e53760ebe9ec3a641a5a
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-05-04 22:03:09 +0300

    preparing for DeepSearch firebase words creation

--- src/App/comps/Dashboard/DeepSearch/DeepSearch.tsx
+++ src/App/comps/Dashboard/DeepSearch/DeepSearch.tsx
@@ -1,11 +1,24 @@
+import { setDoc, doc } from 'firebase/firestore';
 import React from 'react'
 import { useSelector } from 'react-redux';
-
+import { db } from '../../../API/firebase/firebaseConfig';
 
 type Props = {}
 
 export default function DeepSearch({ }: Props) {
+
   const some = useSelector(state => state.yandexDictionaryTranslates.data);
+
+  let database = db;
+
+  function somefunc(e) {
+    setDoc(doc(database, "users", "user", "appendedwords", 'blabla'), {
+      word: '2',
+      translate: '1'
+    });
+    console.log(e.target.nextElementSibling)
+  }
+
   return (
     <div >
 
@@ -24,14 +37,17 @@ export default function DeepSearch({ }: Props) {
               <div style={{ display: 'flex', justifyContent: 'center' }}><div>{x.pos}</div></div>
               {x.tr.map(x =>
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <div>{x.text}</div>
+                  <div
+                    onClick={(e) => { somefunc(e) }}>{x.text}</div>
                   <div>
-                    <button style={{ marginLeft: '5px', width: '25px', height: '15px', backgroundColor: "green" }}></button>
-                    <button disabled style={{ marginLeft: '5px', width: '15px', height: '15px', backgroundColor: "red" }}></button>
+                    <button
+                      style={{
+                        marginLeft: '5px', width: '25px', height: '15px', backgroundColor: "green"
+                      }}></button>
                   </div>
                 </div>)}
             </div>)}
       </div>
-    </div>
+    </div >
   )
 }
