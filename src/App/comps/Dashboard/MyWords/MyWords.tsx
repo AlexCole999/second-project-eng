0c0958b257ab4ac7d96c0bb51a0dd1128c495a3f
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-05-11 20:38:55 +0300

    MyComponents some prepares

--- src/App/comps/Dashboard/MyWords/MyWords.tsx
+++ src/App/comps/Dashboard/MyWords/MyWords.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { getDoc, doc } from 'firebase/firestore';
+import { db } from './../../../API/firebase/firebaseConfig'
 
 type Props = {}
 
 export default function MyWords({ }: Props) {
+
+
+  const [state, setstate] = useState([])
+
+  const dispatch = useDispatch();
+
+  const user = useSelector(state => state.user?.data?.email || 'guest');
+  const words = useSelector(state => state.wordsFromFirebase);
+
+  async function somefunc() {
+
+    let data = (await getDoc(doc(db, "users", user, 'data', 'words'))).data();
+    dispatch({ type: "ADD_DATA_FROM_FIREBASE", payload: data });
+
+
+    console.log(words, Object.keys(words))
+  }
   return (
-    <div>MyWords</div>
+    <div>
+      MyWords
+      <button onClick={somefunc}></button>
+    </div>
   )
 }
--- src/App/redux/reducers/getWordsFromFirebaseReducer.js
+++ src/App/redux/reducers/getWordsFromFirebaseReducer.js
@@ -3,7 +3,7 @@ let defaultstate = { data: [] }
 const getWordsFromFirebaseReducer = (state = defaultstate, action) => {
   switch (action.type) {
     case "ADD_DATA_FROM_FIREBASE":
-      return { ...state, data: action.payload }
+      return action.payload
     default: return state
   }
 }
