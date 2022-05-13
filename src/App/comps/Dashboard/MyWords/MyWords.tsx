df44d307380f712c38e35f0cefd09f7ae5229e4b
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-05-13 17:13:09 +0300

    MyWords component preparing for new base creating functionality

--- src/App/comps/Dashboard/MyWords/MyWords.tsx
+++ src/App/comps/Dashboard/MyWords/MyWords.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { getDoc, doc } from 'firebase/firestore';
+import { getDoc, doc, setDoc, collection, query, getDocs } from 'firebase/firestore';
 import { db } from './../../../API/firebase/firebaseConfig'
 
 type Props = {}
@@ -9,18 +9,30 @@ type Props = {}
 export default function MyWords({ }: Props) {
 
 
-  const [state, setstate] = useState([])
 
+  const [state, setstate] = useState([])
+  const [inputstate, setinputstate] = useState('')
   const dispatch = useDispatch();
 
   const user = useSelector(state => state.user?.data?.email || 'guest');
   const words = useSelector(state => state.wordsFromFirebase);
 
-  async function somefunc() {
+  useEffect(firebaseWordsRequest, [])
+
+  async function getBases() {
+    const q = collection(db, "users", user, 'bases');
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+    });
+  }
+
+  async function firebaseWordsRequest() {
 
     let data = (await getDoc(doc(db, "users", user, 'data', 'words'))).data();
     dispatch({ type: "ADD_DATA_FROM_FIREBASE", payload: data });
-
 
     [...Object.keys(words)].map(x => console.log(words[x]?.word, words[x].translates.map(x => x.translate)));
 
@@ -33,19 +45,28 @@ export default function MyWords({ }: Props) {
           <div>
             {words[x].translates
               .map(x =>
-                <div>
-                  {x.translate} --- {x.language}
-                </div>)
+                <span>
+                  {x.translate}<span style={{ fontSize: '8px' }}>{x.language.split('-')[1]}</span> |
+                </span>)
             }</div>
         </div>));
     console.log(state)
-
   }
+
+  function addNewBase() {
+    setDoc(doc(db, "users", user, 'bases', inputstate), {})
+      .then(x => console.log('done'))
+  }
+
   return (
     <div>
-      MyWords
-      {state}
-      <button onClick={somefunc}></button>
+      <div>MyWords</div>
+      <div><input type="text" onChange={(e) => setinputstate(e.target.value)} /></div>
+      <button onClick={addNewBase}>addbase</button>
+      <button onClick={getBases}>baseslist</button>
+
+      <div>{state}</div>
+      <button onClick={firebaseWordsRequest}></button>
     </div>
   )
 }
