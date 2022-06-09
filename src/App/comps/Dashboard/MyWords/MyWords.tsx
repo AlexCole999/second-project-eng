19baa233fb3d7a5adeda5afbc908003f12f2d34d
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-06-09 14:26:41 +0300

    done base functionality(appending?changing?displaying)

--- src/App/comps/Dashboard/MyWords/MyWords.tsx
+++ src/App/comps/Dashboard/MyWords/MyWords.tsx
@@ -2,9 +2,10 @@ import React from 'react'
 import './MyWords.scss'
 import { useState, useEffect, useRef } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { getDoc, doc, setDoc, collection, query, getDocs } from 'firebase/firestore';
+import { getDoc, doc, setDoc, collection, getDocs } from 'firebase/firestore';
 import { db } from './../../../API/firebase/firebaseConfig'
 import MyWordsElem from './MyWordsElem/MyWordsElem';
+
 
 type Props = {}
 
@@ -19,9 +20,14 @@ export default function MyWords({ }: Props) {
   const [regexp, setregexp] = useState([])
   const [selectedbase, setselectedbase] = useState('')
   const [newbasename, setnewbasename] = useState('')
+  const [viewedcustombasewords, setviewedcustombasewords] = useState([])
+  const [viewcustombase, setviewcustombase] = useState(false)
+  const [viewedbase, setviewedbase] = useState('')
 
   const basemenu = useRef(null);
   const selectedBaseColumn = useRef(null);
+  const viewedbasecolumn = useRef(null);
+  const custombaseselected = useRef(null);
 
   const filteredWordsArray = [...Object.keys(fullWordsList).filter(x => fullWordsList[x].word.toLowerCase().match(regexp))];
 
@@ -77,7 +83,6 @@ export default function MyWords({ }: Props) {
         </select>
         <br />
         Создать новую базу:
-        <br />
         <input
           type="text"
           placeholder='Желаемое имя новой базы...'
@@ -95,27 +100,96 @@ export default function MyWords({ }: Props) {
         <div>
           Фильтр
         </div>
-        <input
-          type="text"
-          placeholder='Часть искомого слова...'
-          onChange={
-            (e) =>
-              setregexp(new RegExp(e.target.value))
-          }
-        />
+        <div>
+          Показывать только слова из базы
+          <input
+            ref={custombaseselected}
+            type="checkbox"
+            onChange={async (e) => {
+              setviewcustombase(custombaseselected.current.checked);
+              console.log(viewedbasecolumn.current.selectedOptions[0].text);
+              let wordsFromBase = (await getDoc(doc(db, "users", user, 'bases', viewedbasecolumn.current.selectedOptions[0].text)));
+              setviewedcustombasewords(
+                [...Object.keys(wordsFromBase.data()).filter(x => wordsFromBase.data()[x].word.toLowerCase())]
+              );
+
+              setviewedbase(viewedbasecolumn.current.selectedOptions[0].text)
+            }
+            }>
+          </input>:
+          <select
+            onChange={async (e) => {
+              let wordsFromBase = (await getDoc(doc(db, "users", user, 'bases', e.target.value)));
+              setviewedcustombasewords(
+                [...Object.keys(wordsFromBase.data()).filter(x => wordsFromBase.data()[x].word.toLowerCase())]
+              )
+
+              setviewedbase(e.target.value)
+              console.log(wordsFromBase.data(), viewedcustombasewords)
+            }
+            }
+            ref={viewedbasecolumn}>
+            {
+              basesList
+                .map(x =>
+                  <option key={x} value={x}>
+                    {x}
+                  </option>)
+            }
+          </select>
+        </div>
+        <div>
+          Слово:
+          <input
+            type="text"
+            placeholder='Часть искомого слова...'
+            onChange={
+              (e) =>
+                setregexp(new RegExp(e.target.value))
+            }
+          />
+        </div>
       </div >
+
+      <div
+        style={{ display: 'flex', justifyContent: 'center', fontSize: '25px', fontWeight: 'bold', marginBottom: '10px' }}>
+        <div>
+          {viewcustombase ? `Слова из базы ${viewedbase}` : 'Общая база'}
+        </div>
+      </div>
+
       <div className='MyWords__words'>
         {
-          filteredWordsArray
-            .map(
-              mainWord =>
-                <MyWordsElem
-                  fullWordsList={fullWordsList}
-                  selectedbase={selectedbase}
-                  element={mainWord}
-                  key={mainWord}
-                />
+          viewcustombase
+            ? (
+              viewedcustombasewords.length
+                ? viewedcustombasewords.map(
+                  mainWord =>
+                    <MyWordsElem
+                      fullWordsList={fullWordsList}
+                      selectedbase={selectedbase}
+                      element={mainWord}
+                      key={mainWord}
+                    />
+
+                )
+                : <div>
+                  <div>
+                    Не найдено слов в базе или некорректно выбрана база, пожалуйста, попробуйте выбрать другую
+                  </div>
+                </div>
             )
+            :
+            filteredWordsArray
+              .map(
+                mainWord =>
+                  <MyWordsElem
+                    fullWordsList={fullWordsList}
+                    selectedbase={selectedbase}
+                    element={mainWord}
+                    key={mainWord}
+                  />
+              )
         }
       </div>
     </div >
