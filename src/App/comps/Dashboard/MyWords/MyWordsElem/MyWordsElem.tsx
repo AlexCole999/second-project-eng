9b32eb2cb1cb079a7cff551d81af5327a8bacecc
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-06-09 11:46:18 +0300

    appended doned function into MyWordsElem component from DeepSearch--->>>ResultRow component

--- src/App/comps/Dashboard/MyWords/MyWordsElem/MyWordsElem.tsx
+++ src/App/comps/Dashboard/MyWords/MyWordsElem/MyWordsElem.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import './MyWordsElem.scss'
-import { useSelector } from 'react-redux';
 import capitalizeFirstLetter from './../../../../functions/capitalizeFirstLetter';
+import { db } from '../../../../API/firebase/firebaseConfig'
+import { useSelector } from 'react-redux';
+import { getDoc, setDoc, doc } from 'firebase/firestore';
+
+let database = db;
 
 type Props = {
   fullWordsList: any,
@@ -10,6 +14,38 @@ type Props = {
 }
 
 export default function MyWordsElem({ fullWordsList, element, selectedbase }: Props) {
+
+  const user = useSelector(state => state.user?.data?.email || 'guest');
+
+  async function addTranslateToDefaultFirebase() { // функция добавления слова в базу
+
+    let oldWords = (await getDoc(doc(database, "users", user, 'data', 'words'))); // запрашиваем данные о словах пользователя с сервера
+
+    if (oldWords.data() == undefined) {  // если добавленных слов по этому пользователю нет
+      await setDoc(doc(database, "users", user, 'data', 'words'), {}); //создаем пустой объект по этому пути
+      oldWords = (await getDoc(doc(database, "users", user, 'data', 'words'))); // заново запрашиваем отправленные данные с сервера
+    }
+
+    let newbase = oldWords.data(); // копируем полученные данные в новый объект, его мы будем изменять для отправки изменений на сервер
+    let newbasewords = newbase; // создаем новый объект на базе уже существующего, его мы будем изменять и отправлять изменения на сервер
+
+    if (!newbasewords[word]) { // если объект не содержит нашего слова, тогда:
+      newbasewords[word] = {} // создаем новое поле, называем его словом, которое переводим, закидываем туда пустой объект 
+      newbasewords[word]['word'] = word // в новосозданном пустом объекте создаем поле с названием 'word', закидываем туда слово, которое переводим
+      newbasewords[word]['translates'] = [
+        { language: selectedLanguage, translate: translate }
+      ] // в новосозданном пустом объекте создаем поле с названием 'translates', закидываем туда новый массив, в массив кидаем новый объект, в объекте прокидываем поле с языковой парой перевода и поле со значение перевода
+    }
+
+    newbasewords[word]['translates'] = [ // если в запрошенных данных есть поле с нашим словом,то идем в поле с его переводами
+      ...newbasewords[word]['translates'] // разворачиваем все старые объекты с переводами в начало нового массива, при этом --->>>
+        .filter(x => x.translate !== translate), // --->>> фильтруем старые переводы, выкидывая из них тот, который хотим добавить (это нужно для устранения дубликатов объектов с одинаковыми переводами)
+      { language: selectedLanguage, translate: translate } // добавляем в конец отфильтрованного массива объект с нашим новым переводом 
+    ]
+
+    setDoc(doc(database, "users", user, 'data', 'words'), newbase).then(x => console.log('appended')); // сформированный и измененный объект newbase отправляем на сервер в качестве новых данных
+
+  }
 
   return (
     <div className='MyWords__elem'>
