4a34216943c906c45b6a7bb914b4cbdf6d921d90
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-06-11 16:23:24 +0300

    some fixes in baseappend fucntion in ResultRow component

--- src/App/comps/Dashboard/DeepSearch/ResultColumn/ResultRow/ResultRow.tsx
+++ src/App/comps/Dashboard/DeepSearch/ResultColumn/ResultRow/ResultRow.tsx
@@ -23,32 +23,46 @@ export default function ResultRow({ translate, examples, sameWords, synonyms, fr
   const user = useSelector(state => state.user?.data?.email || 'guest');
 
   async function addTranslateToFirebase() { // функция добавления слова в базу
+
     let oldWords = (await getDoc(doc(db, "users", user, 'data', 'words'))); // запрашиваем данные о словах пользователя с сервера
     if (oldWords.data() == undefined) {  // если добавленных слов по этому пользователю нет
       await setDoc(doc(db, "users", user, 'data', 'words'), {}); //создаем пустой объект по этому пути
       oldWords = (await getDoc(doc(db, "users", user, 'data', 'words'))); // заново запрашиваем отправленные данные с сервера
     }
-    let newbase = oldWords.data(); // копируем полученные данные в новый объект, его мы будем изменять для отправки изменений на сервер
-    let newbasewords = newbase; // создаем новый объект на базе уже существующего, его мы будем изменять и отправлять изменения на сервер
+
+    let newbasewords = oldWords.data(); // копируем полученные данные в новый объект, его мы будем изменять для отправки изменений на сервер
     if (!newbasewords[word]) { // если объект не содержит нашего слова, тогда:
       newbasewords[word] = {} // создаем новое поле, называем его словом, которое переводим, закидываем туда пустой объект 
       newbasewords[word]['word'] = word // в новосозданном пустом объекте создаем поле с названием 'word', закидываем туда слово, которое переводим
-      newbasewords[word]['translates'] = [
-        { language: selectedLanguage, translate: translate }
-      ] // в новосозданном пустом объекте создаем поле с названием 'translates', закидываем туда новый массив, в массив кидаем новый объект, в объекте прокидываем поле с языковой парой перевода и поле со значение перевода
+      newbasewords[word]['translates'] = [{ language: selectedLanguage, translate: translate }] // в новосозданном пустом объекте создаем поле с названием 'translates', закидываем туда новый массив, в массив кидаем новый объект, в объекте прокидываем поле с языковой парой перевода и поле со значение перевода
     }
-    newbasewords[word]['translates'] = [ // если в запрошенных данных есть поле с нашим словом,то идем в поле с его переводами
-      ...newbasewords[word]['translates'] // разворачиваем все старые объекты с переводами в начало нового массива, при этом --->>>
-        .filter(x => x.translate !== translate), // --->>> фильтруем старые переводы, выкидывая из них тот, который хотим добавить (это нужно для устранения дубликатов объектов с одинаковыми переводами)
-      { language: selectedLanguage, translate: translate } // добавляем в конец отфильтрованного массива объект с нашим новым переводом 
+    newbasewords[word]['translates'] = [...newbasewords[word]['translates'] // разворачиваем все старые объекты с переводами в начало нового массива, при этом --->>>
+      .filter(
+        x => x.translate !== translate   // --->>> фильтруем старые переводы, выкидывая из них тот, который хотим добавить (это нужно для устранения дубликатов объектов с одинаковыми переводами)
+      ),
+    { language: selectedLanguage, translate: translate } // добавляем в конец отфильтрованного массива объект с нашим новым переводом 
     ]
-    setDoc(doc(db, "users", user, 'data', 'words'), newbase).then(x => console.log('appended')); // сформированный и измененный объект newbase отправляем на сервер в качестве новых данных
+
+    setDoc(
+      doc(
+        db, "users", user, 'data', 'words'),
+      newbasewords)
+      .then(
+        x => console.log('appended')
+      ); // сформированный и измененный объект newbase отправляем на сервер в качестве новых данных
+
   }
 
   async function setWordGame() {
-    let allWords = await getDoc(doc(db, "users", user, 'data', 'words'));
-    allWords = allWords.data()
-    console.log({ ...allWords[word], game: translate })
+
+    const allWords = await getDoc(doc(db, "users", user, 'data', 'words'));
+    allWords = allWords.data();
+
+    const theword = allWords[word];
+    theword['game'] = translate
+
+    console.log({ ...allWords, theword })
+
   }
 
   return (
