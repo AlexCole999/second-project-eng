a3ffb09f985c2fd53a294489f4676b86bfdc768a
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-06-16 14:49:10 +0300

    appended useEffect hook with firebase all words request when SPA has started

--- src/App/comps/App.tsx
+++ src/App/comps/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useEffect } from 'react'
 import './App.scss'
 import Nav from './Nav/Nav';
 import Dashboard from './Dashboard/Dashboard';
-
+import { getDoc, doc } from 'firebase/firestore';
+import { useDispatch, useSelector } from 'react-redux';
+import { db } from '../API/firebase/firebaseConfig'
 
 type Props = {}
 
 export default function App({ }: Props) {
+
+  const dispatch = useDispatch()
+
+  const user = useSelector(state => state.user?.data?.email || 'guest');
+
+  useEffect(() => {
+    getDoc(doc(db, "users", user, 'data', 'words')).then(data => {
+      dispatch({
+        type: "ADD_DATA_FROM_FIREBASE",
+        payload: data.data()
+      });
+    });
+  }, [])
 
   return (
 
