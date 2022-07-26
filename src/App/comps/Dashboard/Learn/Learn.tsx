92d85cec364abe7b4c0d00b84e6fc65c4439bafa
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-07-26 16:58:32 +0300

    preparing for Learn component

--- src/App/comps/Dashboard/Learn/Learn.tsx
+++ src/App/comps/Dashboard/Learn/Learn.tsx
@@ -6,33 +6,138 @@ type Props = {}
 
 export default function Learn({ }: Props) {
 
-  const [wordinbase, setwordinbase] = useState('')
-  const [wordininput, setwordininput] = useState('')
-  const [state, setstate] = useState([])
+  const [inguesswordgame, setinguesswordgame] = useState(false)
+  const [inguesstranslategame, setinguesstranslategame] = useState(false)
 
-  return (
-    <div className='Learn'>
-      Learn
+  const StartGameMenu = () => {
 
-      <input type="text" onChange={(e) => setwordinbase(e.target.value)} placeholder='wordinbase' />
-      <input type="text" onChange={(e) => setwordininput(e.target.value)} placeholder='wordininput' />
+    return (
 
-      <div>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
 
-        {
-          wordinbase
-            .split('')
-            .map((elem, index) =>
-              elem == wordininput[index]
-                ? <span style={{ color: 'green' }}>{wordininput[index]}</span>
-                : <span style={{ color: 'red' }} >{wordininput[index]}</span>
-            )
-        }
+        <div style={{ fontStyle: 'italic' }}>
+          <div style={{ textAlign: 'center' }}>
+            <b style={{ fontSize: '2.3em', textDecoration: 'underline' }}>Отгадай перевод</b><br />
+            <div style={{ fontSize: '1.3em' }}>Выпадает случайное слово из базы, вы угадываете перевод этого слова</div>
+          </div>
+        </div>
+
+        <button
+          style={{ width: '220px', height: '60px', backgroundColor: 'green', borderRadius: '15px', color: 'white', border: 'none', margin: '15px 0px 15px 0px', fontSize: '1.2em', fontWeight: 'bold' }}
+          onClick={() => setinguesstranslategame(!inguesstranslategame)}
+        >
+          ИГРАТЬ
+        </button>
+
+        <div style={{ fontStyle: 'italic' }}>
+          <div style={{ textAlign: 'center' }}>
+            <b style={{ fontSize: '2.3em', textDecoration: 'underline' }}>Отгадай слово</b><br />
+            <div style={{ fontSize: '1.3em' }}>Выпадает случайный перевод из базы, вы угадываете это слово на основном языке</div>
+          </div>
+        </div>
+
+        <button
+          style={{ width: '220px', height: '60px', backgroundColor: 'green', borderRadius: '15px', color: 'white', border: 'none', margin: '15px 0px 15px 0px', fontSize: '1.2em', fontWeight: 'bold' }}
+          onClick={() => setinguesswordgame(!inguesswordgame)}
+        >
+          ИГРАТЬ
+        </button>
 
       </div>
 
-      <button style={{ width: '250px', height: '60px', backgroundColor: 'green', borderRadius: '15px', color: 'white', border: 'none', marginTop: '15px', fontSize: '26px', fontWeight: 'bold' }}>ИГРАТЬ</button>
+    )
+
+  }
+
+  const GuessWordGame = () => {
+
+    const [wordinbase, setwordinbase] = useState('')
+    const [wordininput, setwordininput] = useState('')
+
+    return (
+
+      <div>
+
+        <input type="text" onChange={(e) => setwordinbase(e.target.value)} placeholder='wordinbase' />
+        <input type="text" onChange={(e) => setwordininput(e.target.value)} placeholder='wordininput' />
+
+        <div>
+          {
+            wordinbase
+              .split('')
+              .map((elem, index) =>
+                elem == wordininput[index]
+                  ? <span style={{ color: 'green' }}>{wordininput[index]}</span>
+                  : <span style={{ color: 'red' }} >{wordininput[index]}</span>
+              )
+          }
+        </div>
+
+        <button
+          style={{ width: '200px', height: '50px', backgroundColor: 'red', borderRadius: '15px', color: 'white', border: 'none', margin: '15px 0px 15px 0px', fontSize: '1.2em', fontWeight: 'bold' }}
+          onClick={() => setinguesswordgame(!inguesswordgame)}
+        >
+          ЗАКОНЧИТЬ
+        </button>
+
+      </div>
+
+    )
+
+  }
+
+  const GuessTranslateGame = () => {
+
+    const [wordinbase, setwordinbase] = useState('')
+    const [wordininput, setwordininput] = useState('')
+
+    return (
+
+      <div>
+
+        <input type="text" onChange={(e) => setwordinbase(e.target.value)} placeholder='wordinbase' />
+        <input type="text" onChange={(e) => setwordininput(e.target.value)} placeholder='wordininput' />
+
+        <div>
+          {
+            wordinbase
+              .split('')
+              .map((elem, index) =>
+                elem == wordininput[index]
+                  ? <span style={{ color: 'green' }}>{wordininput[index]}</span>
+                  : <span style={{ color: 'red' }} >{wordininput[index]}</span>
+              )
+          }
+        </div>
+
+        <button
+          style={{ width: '200px', height: '50px', backgroundColor: 'red', borderRadius: '15px', color: 'white', border: 'none', margin: '15px 0px 15px 0px', fontSize: '1.2em', fontWeight: 'bold' }}
+          onClick={() => setinguesstranslategame(!inguesstranslategame)}
+        >
+          ЗАКОНЧИТЬ
+        </button>
+
+      </div>
+
+    )
+
+  }
+
+  return (
+
+    <div className='Learn'>
+
+      {
+        inguesswordgame
+          ? <GuessWordGame />
+          : inguesstranslategame
+            ? <GuessTranslateGame />
+            : <StartGameMenu />
+
+      }
 
     </div>
+
   )
+
 }
