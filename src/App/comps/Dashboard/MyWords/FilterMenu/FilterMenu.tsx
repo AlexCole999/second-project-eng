198a0926ea5c2b21bcd970b0151acaeb1a6f8dd9
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-08-08 23:09:30 +0300

    delete unnecessary React.memo(FilterMenu)

--- src/App/comps/Dashboard/MyWords/FilterMenu/FilterMenu.tsx
+++ src/App/comps/Dashboard/MyWords/FilterMenu/FilterMenu.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './FilterMenu.scss'
 
-const FilterMenu = ({ setregexp }) => {
+export default function FilterMenu({ setregexp }) {
 
   return (
 
@@ -27,4 +27,3 @@ const FilterMenu = ({ setregexp }) => {
   )
 
 }
-export default React.memo(FilterMenu)
