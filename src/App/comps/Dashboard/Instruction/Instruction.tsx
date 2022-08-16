e0f2d20c56737ab24f5bee5e42b24689243fe27a
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-08-16 16:41:44 +0300

    tried to use SpeechRecognition. Succesfull. Made some functionality in Instruction component for the moment, i will transfer all the stuff into NavSearch later

--- src/App/comps/Dashboard/Instruction/Instruction.tsx
+++ src/App/comps/Dashboard/Instruction/Instruction.tsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './Instruction.scss'
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 type Props = {}
 
 export default function Instruction({ }: Props) {
-  return (
-    <div className='Instruction'>Instruction</div>
+
+  const {
+    transcript,
+    resetTranscript,
+    listening,
+    browserSupportsSpeechRecognition
+  } = useSpeechRecognition();
+
+  const startListening = () => SpeechRecognition.startListening({ continuous: true });
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
+  const input = useRef()
+
+  useEffect(
+    () => {
+      let searchedValue = transcript.match(/[а-яА-Яa-zA-Z]+$/i) ? transcript.match(/[а-яА-Яa-zA-Z]+$/i)[0] : ''
+      input.current.value = searchedValue;
+      console.log(searchedValue)
+    },
+    [transcript]
   )
-}
+
+  return (
+    <div className='Instruction'>
+      <p>Microphone: {listening ? 'on' : 'off'}</p>
+      <button
+        onTouchStart={startListening}
+        onMouseDown={startListening}
+        onTouchEnd={SpeechRecognition.stopListening}
+        onMouseUp={SpeechRecognition.stopListening}
+      >Hold to talk</button>
+      <button
+        onClick={() => { console.log(SpeechRecognition.getRecognition()) }}
+      >1</button>
+      <p>{transcript}</p>
+      <input type="text" ref={input} />
+    </div>
+  );
+};
