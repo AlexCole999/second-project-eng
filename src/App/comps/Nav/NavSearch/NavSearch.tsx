a130c31d3da673e6439a7fbeac38ab61e05fd593
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-08-16 19:51:05 +0300

    appended microphone functionality in NavSearch component

--- src/App/comps/Nav/NavSearch/NavSearch.tsx
+++ src/App/comps/Nav/NavSearch/NavSearch.tsx
@@ -1,16 +1,21 @@
 import './NavSearch.scss';
 import React from 'react';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import debounce from './../../../functions/debounce';
 import { useDispatch, useSelector } from 'react-redux';
 import { AiOutlineClose } from "react-icons/ai";
+import { FaMicrophone } from "react-icons/fa";
 import SearchedMainWord from './SearchedMainWord/SearchedMainWord';
 import LanguagesPanel from './LanguagesPanel/LanguagesPanel';
 import yandexDictionaryRequest from './../../../Api/yandexDictionary/yandexDictionaryRequest';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 type Props = {}
 
 export default function NavSearch({ }: Props) {
+
+  const { transcript, listening } = useSpeechRecognition();
+  const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-US' });
 
   const dispatch = useDispatch();
 
@@ -29,9 +34,28 @@ export default function NavSearch({ }: Props) {
 
   const debouncedYandexDictionaryInputRequest = debounce(yandexDictionaryInputRequest, 500)
 
+  useEffect(() => {
+    let searchedValue = transcript.match(/[а-яА-Яa-zA-Z]+$/i) ? transcript.match(/[а-яА-Яa-zA-Z]+$/i)[0] : ''
+    inputsearch.current.value = searchedValue;
+    setinputstate(searchedValue);
+    yandexDictionaryRequest(selectedLanguage, searchedValue)
+      .then(response => {
+        dispatch({ type: "GET_TRANSLATES_FROM_YANDEX_DICTIONARY", payload: response.data.def });
+      })
+  }, [transcript])
+
   return (
 
     <div className="NavSearch">
+
+      <div className="NavSearch__microphoneButton" style={{ backgroundColor: listening ? 'red' : 'blue' }}
+        onTouchStart={startListening}
+        onMouseDown={startListening}
+        onTouchEnd={SpeechRecognition.stopListening}
+        onMouseUp={SpeechRecognition.stopListening}
+      >
+        <FaMicrophone color='white' size={20} />
+      </div>
 
       <div className='NavSearch__inputElem'>
 
