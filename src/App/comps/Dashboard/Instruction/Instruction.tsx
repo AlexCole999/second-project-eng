6de5ea4fcb5efa3c23a41b38e328aeaa76a40d9a
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-08-17 13:18:17 +0300

    added separated logic for elements within Instruction component

--- src/App/comps/Dashboard/Instruction/Instruction.tsx
+++ src/App/comps/Dashboard/Instruction/Instruction.tsx
@@ -16,101 +16,131 @@ type Props = {}
 
 export default function Instruction({ }: Props) {
 
+  function InstructionTitle({ title }) {
+    return (
+      <div className='Instruction__title'>
+        {title}
+      </div>
+    )
+  }
+
+  function InstructionSubtitle({ subtitle }) {
+    return (
+      <div className='Instruction__subtitle'>
+        {subtitle}
+      </div>
+    )
+  }
+
+  function InstructionElem({ direction, icon, title, text }) {
+    return (
+      <div className='Instruction__elem'>
+        <NavLink to={direction}>{icon}</NavLink>
+        <div className='Instruction__elemtext'>
+          <b>{title}:</b>
+          <div>{text}</div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div className='Instruction'>
 
-      <div style={{ fontSize: '35px', fontWeight: 'bolder', textAlign: 'center', marginBottom: '15px', marginTop: '15px' }}>
-        Инструкция
-      </div>
+      <InstructionTitle title='Инструкция' />
 
-      <div style={{ fontSize: '25px', fontWeight: 'bolder', textAlign: 'center', marginBottom: '15px', marginTop: '15px' }}>
-        Профиль
-      </div>
+      <InstructionSubtitle subtitle='Профиль' />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <NavLink to={"/Profile"}>
-          <FiUserX size={60} color='black' />
-        </NavLink>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Профиль:</b> нажмите на кнопку входа слева вверху, чтобы зайти в свой профиль. Все данные будут сохраняться в индивидуальной базе, будет доступна личная статистика и список слов</div>
-      </div>
+      <InstructionElem
+        direction='/Profile'
+        icon={<FiUserX size={60} color='black' />}
+        title='Профиль'
+        text='Нажмите на кнопку входа слева вверху, чтобы зайти в свой профиль. Все данные будут сохраняться в индивидуальной базе, будет доступна личная статистика и список слов'
+      />
 
-      <div style={{ fontSize: '25px', fontWeight: 'bolder', textAlign: 'center', marginBottom: '15px', marginTop: '15px' }}>
-        Поиск переводов
-      </div>
+      <InstructionSubtitle subtitle='Поиск переводов' />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <NavLink to={"/DeepSearch"}>
-          <FaMicrophone size={60} color='black' />
-        </NavLink>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Голосовой ввод:</b> нажмите и удерживайте кнопку голосового ввода, скажите слово в слух, затем отпустите кнопку.Строка поиска заполнится сказанным словом, произойдет поиск переводов.(Примечание: По умолчанию распознаватель текста настроен на идентификацию английской речи, однако вы можете изменить это во вкладке настроек. Независимо от количества произнесенных слов определяться будет только самое последнее)</div>
-      </div>
+      <InstructionElem
+        direction='/DeepSearch'
+        icon={<FaMicrophone size={60} color='black' />}
+        title='Голосовой ввод'
+        text='Нажмите и удерживайте кнопку голосового ввода, скажите слово в слух, затем отпустите кнопку.Строка поиска заполнится сказанным словом, произойдет поиск переводов.(Примечание: По умолчанию распознаватель текста настроен на идентификацию английской речи, однако вы можете изменить это во вкладке настроек. Независимо от количества произнесенных слов определяться будет только самое последнее)'
+      />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Текстовый ввод:</b> ниже кнопки голосового ввода находится поле текстового ввода. Введите слово в текстовом формате, чтобы найти перевод</div>
-      </div>
+      <InstructionElem
+        direction={''}
+        icon={null}
+        title='Текстовый ввод'
+        text='Ниже кнопки голосового ввода находится поле текстового ввода. Введите слово в текстовом формате, чтобы найти перевод'
+      />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Быстрый результат:</b> для отображения самого популярного перевода непосредственно в панели поиска - раскройте панель меню (кликните на две направленные горизонтально вправо стрелки, расположенные выше и левее иконки входа в аккаунт: <FiChevronsRight />). Результат поиска будет отображен ниже панели текстового ввода. (Примечание: доступно только при ширине экрана больше 550 пикселей. Поверните экран горизонтально для отображения быстрого результата, либо воспользуйтесь вкладкой Продвинутого поиска)</div>
-      </div>
+      <InstructionElem
+        direction={''}
+        icon={null}
+        title='Быстрый результат'
+        text='Для отображения самого популярного перевода непосредственно в панели поиска - раскройте панель меню (кликните на две направленные горизонтально вправо стрелки, расположенные выше и левее иконки входа в аккаунт). Результат поиска будет отображен ниже панели текстового ввода. (Примечание: доступно только при ширине экрана больше 550 пикселей. Поверните экран горизонтально для отображения быстрого результата, либо воспользуйтесь вкладкой Продвинутого поиска)'
+      />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Кнопки быстрого добавления:</b> ниже быстрого результата отобразятся кнопки управления словом: <AiFillCheckCircle />, <AiFillPlayCircle />. Добавьте перевод в базу, кликнув на кнопку с отображением галочки. Добавьте слово в игровые, кликнув по иконке с изображением "play" (Примечание:При повторном нажатии на кнопку с галочкой перевод удалится, однако, если в базе слов есть иные переводы - она останется зеленой, сообщая о наличии слова в базе)</div>
-      </div>
+      <InstructionElem
+        direction={''}
+        icon={null}
+        title='Кнопки быстрого добавления'
+        text='Ниже строки быстрого результата отобразятся кнопки управления словом. Добавьте перевод в базу, кликнув на кнопку с отображением галочки. Добавьте слово в игровые, кликнув по иконке с изображением "play" (Примечание:При повторном нажатии на кнопку с галочкой перевод удалится, однако, если в базе слов есть иные переводы - она останется зеленой, сообщая о наличии слова в базе)'
+      />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Языковая панель:</b> для выбора языка раскройте панель языков (кликните на две направленные вертикально вниз стрелки под флагом выбранного языка:<FiChevronsRight style={{ rotate: '90deg' }} />) и выберите интересующий вас язык. Для смены направления перевода нажмите на кнопку, расположенную между кнопкой открытия языковой панели и флагом языка: <FiRotateCw /></div>
-      </div>
+      <InstructionElem
+        direction={''}
+        icon={null}
+        title='Языковая панель'
+        text='Для выбора языка раскройте панель языков (кликните на две направленные вертикально вниз стрелки под флагом выбранного языка и выберите интересующий вас язык. Для смены направления перевода нажмите на кнопку, расположенную между кнопкой открытия языковой панели и флагом языка'
+      />
 
-      <div style={{ fontSize: '25px', fontWeight: 'bolder', textAlign: 'center', marginBottom: '15px', marginTop: '15px' }}>
-        Кнопки навигации
-      </div>
+      <InstructionSubtitle subtitle='Кнопки навигации' />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <NavLink to={"/DeepSearch"}>
-          <FiZoomIn size={60} color='black' />
-        </NavLink>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Продвинутый поиск:</b> откройте вкладку, чтобы увидеть все варианты переводов введенного слова, информацию о примерах и частоте их использования</div>
-      </div>
+      <InstructionElem
+        direction={"/DeepSearch"}
+        icon={<FiZoomIn size={60} color='black' />}
+        title='Продвинутый поиск'
+        text='Откройте вкладку, чтобы увидеть все варианты переводов введенного слова, информацию о примерах и частоте их использования'
+      />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <NavLink to={"/Learn"} title="">
-          <FiPlay size={60} color='black' />
-        </NavLink>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Игра:</b> откройте вкладку, чтобы выбрать одну из игр для изучения слов</div>
-      </div>
+      <InstructionElem
+        direction={"/Learn"}
+        icon={<FiPlay size={60} color='black' />}
+        title='Игра'
+        text='Откройте вкладку, чтобы выбрать одну из игр для изучения слов'
+      />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <NavLink to={"/MyWords"}>
-          <FiList size={60} color='black' />
-        </NavLink>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>База слов:</b> откройте вкладку, чтобы увидеть все ранее добавленные слова, удалить слова, удалить отедльные переводы слов или выбрать новое слово для игр</div>
-      </div>
+      <InstructionElem
+        direction={"/MyWords"}
+        icon={<FiList size={60} color='black' />}
+        title='База слов'
+        text='Откройте вкладку, чтобы увидеть все ранее добавленные слова, удалить слова, удалить отедльные переводы слов или выбрать новое слово для игр'
+      />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <NavLink to={"/Statistics"}>
-          <FiTrendingUp size={60} color='black' />
-        </NavLink>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Статистика:</b> откройте вкладку, чтобы увидеть количество добавленных слов, переводов, а так же сыгранных и успешно завершенных игр</div>
-      </div>
+      <InstructionElem
+        direction={"/Statistics"}
+        icon={<FiTrendingUp size={60} color='black' />}
+        title='Статистика'
+        text='Откройте вкладку, чтобы увидеть количество добавленных слов, переводов, а так же сыгранных и успешно завершенных игр'
+      />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <NavLink to={"/Settings"}>
-          <FiSliders size={60} color='black' />
-        </NavLink>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}>
-          <b>
-            Настройки:
-          </b> откройте вкладку, чтобы изменить цветовой режим и некоторые расширенные параметры поиска
-        </div>
-      </div>
+      <InstructionElem
+        direction={"/Settings"}
+        icon={<FiSliders size={60} color='black' />}
+        title='Настройки'
+        text='Откройте вкладку, чтобы изменить цветовой режим и некоторые расширенные параметры поиска'
+      />
 
-      <div style={{ display: 'flex', alignItems: 'center', marginBottom: '15px' }}>
-        <NavLink to={"/Instruction"}>
-          <BsQuestionLg size={60} color='black' />
-        </NavLink>
-        <div style={{ display: 'inline-block', fontStyle: 'italic', marginLeft: '10px' }}><b>Инструкция</b> откройте вкладку, чтобы снова увидеть данную инструкцию</div>
-      </div>
+      <InstructionElem
+        direction={"/Instruction"}
+        icon={<BsQuestionLg size={60} color='black' />}
+        title='Инструкция'
+        text='Откройте вкладку, чтобы снова увидеть данную инструкцию'
+      />
 
     </div>
+
   );
+
 };
