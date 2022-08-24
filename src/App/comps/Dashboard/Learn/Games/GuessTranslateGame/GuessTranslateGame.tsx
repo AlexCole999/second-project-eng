2aa50476e3b8193370cb4e69b14f228150b5cf15
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-08-24 19:00:22 +0300

    some prepares for first game

--- src/App/comps/Dashboard/Learn/Games/GuessTranslateGame/GuessTranslateGame.tsx
+++ src/App/comps/Dashboard/Learn/Games/GuessTranslateGame/GuessTranslateGame.tsx
@@ -8,57 +8,72 @@ export default function GuessTranslateGame({ endgame }) {
 
   const base = useSelector(state => state.allWordsFromFirebase)
   const gamebase = Object.keys(base).filter(x => base[x]?.gameword)
-  const [changedWord, setchangedWord] = useState('НЕИЗВЕСТНО')
+  const [changedWord, setchangedWord] = useState('')
   const [wordinbase, setwordinbase] = useState('')
   const [wordininput, setwordininput] = useState('')
 
+  function getRandomWordGromBase() {
+    return gamebase[Math.ceil(Math.random() * gamebase.length - 1)]
+  }
+
   return (
 
-    <div>
+    <div className='GuessTranslateGame'>
 
-      <input type="text" onChange={(e) => setwordinbase(e.target.value)} placeholder='wordinbase' />
-      <input type="text" onChange={(e) => setwordininput(e.target.value)} placeholder='wordininput' />
-      <br />
+      <div className='GuessTranslateGame__body'>
 
-      <button style={{ width: '50px', height: '25px' }} onClick={() => { console.log(gamebase); setchangedWord(gamebase[Math.ceil(Math.random() * gamebase.length - 1)]) }}></button>
-      <button style={{ width: '50px', height: '25px' }} onClick={() => { console.log(base[changedWord]); }}></button>
-      <br />
+        <button className='GuessTranslateGame__getNewWordButton' onClick={() => { setchangedWord(getRandomWordGromBase()) }}>
+          НОВОЕ СЛОВО
+        </button>
 
-      {changedWord.split('').map(x => <span style={{ fontSize: '85px' }}>{x}</span>)}
+        <div className='GuessTranslateGame__gameword'>
+          {
+            changedWord
+              .toUpperCase()
+              .split('')
+              .map(
+                x =>
+                  <span className='GuessTranslateGame__gamewordLetter'>
+                    {x}
+                  </span>)
+          }
+        </div>
 
-      <br />
 
-      {
-        <div style={{ display: 'flex' }}>
+        <div className='GuessTranslateGame__translate'>
+
           {base[changedWord]
             ?.gameword
+            .toUpperCase()
             .split('')
             .map(x =>
-              <div style={{ width: '50px', minHeight: '75px', border: '2px solid black', borderRadius: '15px', fontSize: '85px', marginLeft: '25px' }}>{ }</div>
+              <div className='GuessTranslateGame__translateLetterBox'>
+                <div className='GuessTranslateGame__translateLetter'>{x}</div>
+              </div>
             )}
+
         </div>
-      }
 
-      <div>
 
-        {
-          wordinbase
-            .split('')
-            .map((elem, index) =>
-              elem == wordininput[index]
-                ? <span style={{ color: 'green' }}>{wordininput[index]}</span>
-                : <span style={{ color: 'red' }} >{wordininput[index]}</span>
-            )
-        }
+        {/* <div>
+
+          {
+            wordinbase
+              .split('')
+              .map((elem, index) =>
+                elem == wordininput[index]
+                  ? <span style={{ color: 'green' }}>{wordininput[index]}</span>
+                  : <span style={{ color: 'red' }} >{wordininput[index]}</span>
+              )
+          }
+
+        </div> */}
+
+        <button className='GuessTranslateGame__endGameButton' onClick={endgame}>
+          STOP
+        </button>
 
       </div>
-
-      <button
-        style={{ width: '200px', height: '50px', backgroundColor: 'red', borderRadius: '15px', color: 'white', border: 'none', margin: '15px 0px 15px 0px', fontSize: '1.2em', fontWeight: 'bold' }}
-        onClick={endgame}
-      >
-        ЗАКОНЧИТЬ
-      </button>
 
     </div>
 
