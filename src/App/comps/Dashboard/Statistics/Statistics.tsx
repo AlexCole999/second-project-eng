af4217d20af7a0aacf937ffdc9fdf24e05634e8d
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-08-25 14:06:07 +0300

    created statistics data for Statistics component

--- src/App/comps/Dashboard/Statistics/Statistics.tsx
+++ src/App/comps/Dashboard/Statistics/Statistics.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Statistics.scss'
+import { useSelector } from 'react-redux';
 
 type Props = {}
 
 export default function Statistics({ }: Props) {
+  console.log('render')
+
+  const allWordsFromFirebase = useSelector(state => state.allWordsFromFirebase)
+
+  const wordsCount = Object.keys(allWordsFromFirebase).length
+  const [translatesCount, settranslatesCount] = useState(0)
+
+  function translatesCounter() {
+    let translatesCount = 0;
+    Object.keys(allWordsFromFirebase).forEach(x => translatesCount += allWordsFromFirebase[x].translates.length)
+    settranslatesCount(translatesCount)
+  }
+
+
+
+  useEffect(() => { translatesCounter() })
+
   return (
-    <div className='Statistics'>Statistics</div>
+    <div className='Statistics'>
+
+      <div>Количество слов в словаре: {wordsCount}</div>
+      <div>Количество переводов в словаре: {translatesCount}</div>
+
+    </div>
   )
 }
