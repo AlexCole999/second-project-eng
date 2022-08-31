772d0892daf5aae11efe9874ec6632277a3e9a3f
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-08-31 17:25:40 +0300

    added components and folders for second game

---
+++ src/App/comps/Dashboard/Learn/Games/GuessGamewordByLettersGame/GuessGamewordByLettersGame.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './GuessGamewordByLettersGame.scss'
+
+type Props = {}
+
+export default function GuessGamewordByLettersGame({ }: Props) {
+  return (
+    <div>GuessGamewordByLettersGame</div>
+  )
+}
