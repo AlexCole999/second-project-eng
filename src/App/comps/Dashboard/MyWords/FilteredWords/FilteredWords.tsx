627e003ef4433b01e984561b989366dd85fb10b7
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-09-07 16:36:49 +0300

    appended GoTopButton

--- src/App/comps/Dashboard/MyWords/FilteredWords/FilteredWords.tsx
+++ src/App/comps/Dashboard/MyWords/FilteredWords/FilteredWords.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './FilteredWords.scss'
 import MyWordsElem from './MyWordsElem/MyWordsElem';
-
+import { BsFillArrowUpCircleFill } from 'react-icons/bs'
 type Props = { filteredWords: any }
 
 export default function FilteredWords({ filteredWords }: Props) {
 
+  const [showTopBtn, setShowTopBtn] = useState(false);
+
+  function GoTopButton() {
+    return (
+      <BsFillArrowUpCircleFill
+        size={35}
+        style={{ position: 'fixed', bottom: '0.5%', right: '1%' }}
+        onClick={() => { window.scrollTo({ top: 0, behavior: 'smooth' }) }}
+      />
+    )
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 400) {
+        setShowTopBtn(true);
+      } else {
+        setShowTopBtn(false);
+      }
+    });
+  }, []);
+
   return (
 
-    <div className='MyWords__words'>
+    <div className='MyWords__words' style={{ position: 'relative' }}>
 
       {
         filteredWords
@@ -20,6 +42,8 @@ export default function FilteredWords({ filteredWords }: Props) {
               />
           )
       }
+
+      {showTopBtn ? <GoTopButton /> : ''}
 
     </div>
 
