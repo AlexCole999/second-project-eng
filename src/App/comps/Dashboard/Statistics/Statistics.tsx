218ad9f991df33121933ca2474746991b3dbba6e
Leo <62186360+AlexCole999@users.noreply.github.com>
2022-09-08 21:02:32 +0300

    added statistics request in Statistics component

--- src/App/comps/Dashboard/Statistics/Statistics.tsx
+++ src/App/comps/Dashboard/Statistics/Statistics.tsx
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import './Statistics.scss'
 import { useSelector } from 'react-redux';
+import { db } from '../../../API/firebase/firebaseConfig';
+import { getDoc, doc } from 'firebase/firestore';
 
 type Props = {}
 
 export default function Statistics({ }: Props) {
-  console.log('render')
+
+  const localStorageUserData = JSON.parse(localStorage.getItem('user'));
 
   const allWordsFromFirebase = useSelector(state => state.allWordsFromFirebase)
+  const user = useSelector(state => state.user?.data?.email || localStorageUserData?.email || 'guest');
+
+  const [gamestatistics, setgamestatistics] = useState({})
 
   const wordsCount = Object.keys(allWordsFromFirebase).length
   let translatesCount = 0;
@@ -20,13 +26,43 @@ export default function Statistics({ }: Props) {
     }
   )
 
+  function getStatisticsFromFirebase() {
+    getDoc(doc(db, "users", user, 'data', 'statistics')).then(x => setgamestatistics(x.data()))
+  }
+
+  useEffect(() => {
+    getStatisticsFromFirebase();
+  }, [])
+
   return (
 
     <div className='Statistics'>
 
-      <div>Слов в игре: {gameWordsCount}</div>
-      <div>Слов в словаре: {wordsCount}</div>
-      <div>Переводов в словаре: {translatesCount}</div>
+      <div>
+
+        <div>Статистика базы:</div><br />
+
+        <div>Слов в игре: {gameWordsCount}</div>
+        <div>Слов в словаре: {wordsCount}</div>
+        <div>Переводов в словаре: {translatesCount}</div>
+
+      </div><br />
+
+      <div>
+
+        <div>Статистика игр:</div><br />
+
+        <div>Напиши слово правильно:</div>
+        <div>Всего сыграно: {gamestatistics?.GuessGamewordByLettersGame?.gamesCount}</div>
+        <div>Верно отгадано: {gamestatistics?.GuessGamewordByLettersGame?.correctGamesCount}</div>
+        <br />
+
+        <div>Напиши перевод правильно:</div>
+        <div>Всего сыграно: {gamestatistics?.GuessTranslationByLettersGame?.gamesCount}</div>
+        <div>Верно отгадано: {gamestatistics?.GuessTranslationByLettersGame?.correctGamesCount}</div>
+        <br />
+
+      </div>
 
     </div>
 
